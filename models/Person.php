c5c3dccec706f3d2f0776065bd6a2f03f65e3455
Matheus E. Muller <hello@memuller.com>
2012-02-08 19:06:26 -0200

    Creating persons automatically persists them.
    Uses a field class definition to write a DB insert query.
    Also uses those definitions to enforce DB schema.

--- models/Person.php
+++ models/Person.php
@@ -3,13 +3,24 @@
 
 	class Person {
 
+		static $table_sufix = 'fcn_people' ;
 		static $table_name ; 
-		static $fields = array('name','email','phone') ;
+		static $fields = array(
+			'name' => array('required' => true,  'size' => 255) ,
+			'email' => array('required' => true,  'unique' => true, 'size' => 255),
+			'phone' => array('size' => 50), 
+			'registered_in' => array('required' => true, 'type' => 'datetime', 'mechanized' => array('date', 'Y-m-d H:i:s'))
+		) ;
+
+		static $belongs_to ;
+		static $has_many ; 
+
+		public $name, $email, $phone, $id, $registered_in ; 
 
 		static function get_table_name(){
 			global $wpdb ;
-			if(!isset(self::$table_name)){
-				self::$table_name = $wpdb->prefix . 'fcn_people' ;
+			if(!isset(static::$table_name)){
+				static::$table_name = $wpdb->prefix . static::$table_sufix ;
 			}
 		}
 
@@ -17,23 +28,62 @@
 			global $wpdb ;
 			
 			require_once ABSPATH . 'wp-admin/includes/upgrade.php' ;
-			self::get_table_name();
+			static::get_table_name();
 
+			$field_definitions = array() ;
+			$field_definitions[]= "id bigint(20) unsigned not null auto_increment" ;
+			$key_definitions = array() ;
+			$key_definitions[] = "primary key id (id)" ;
+
+			foreach (static::$fields as $field_name => $field_options) {
+				if( !isset($field_options['size'])) $field_options['size'] = 255 ;
+				
+				if( !isset($field_options['type'])){
+					$field_type = "varchar(".$field_options['size'].")" ;
+				} else {
+					$field_type = $field_options['type'] ;
+				}
+
+				$required = $field_options['required'] ? "not null" : "null" ;
+				$field_definitions[]= "$field_name $field_type $required" ;
+
+				if($field_options['unique']){
+					$key_definitions[]= "unique key $field_name ($field_name)" ; 
+				}
+			}
 			$sql = sprintf("CREATE TABLE %s (
-				id BIGINT(20) unsigned NOT NULL AUTO_INCREMENT,
-				name VARCHAR(150) NOT NULL,
-				email VARCHAR(150) NOT NULL,
-				phone VARCHAR(50) NULL,
-				registered_in DATETIME NOT NULL,
-				PRIMARY KEY id (id),
-				UNIQUE KEY email (email)
-			);", self::$table_name) ;
-			dbDelta($sql) ;		
+				%s ,
+				%s
+			);", self::$table_name, implode(","."\n", $field_definitions), implode(","."\n", $key_definitions)) ;
+
+			dbDelta($sql) ;
+			 		
 		}
 
-		function __construct($args){
+		function __construct($values, $save = true){			 
 			
+			global $wpdb ; static::get_table_name();
+			if($save === true){
+				$fields = array();
+
+				foreach (static::$fields as $field_name => $field_options) {
+					
+					if(isset($field_options['mechanized'])){
+						$values[$field_name] =  call_user_func($field_options['mechanized'][0], $field_options['mechanized'][1]);
+					}
+
+					$fields[$field_name] = $values[$field_name] ; $this->$field_name = $values[$field_name] ;
+				}
+
+				$wpdb->insert(static::$table_name, $fields) ;
+				$this->id = $wpdb->insert_id ; 
+			} else {
+				foreach ($values as $k => $v) {
+					$this->$k = $v ; 			
+				}		
+			}
 		}
+		
 	}
 
 ?>
