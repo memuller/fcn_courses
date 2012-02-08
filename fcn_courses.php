a2e0a05c7bc843267f55acb864716048ef6ae3fc
Matheus E. Muller <hello@memuller.com>
2012-02-08 20:04:49 -0200

    Bumps DB version.
    0.2, reflects automated schema generator and increases on field sizes.

--- fcn_courses.php
+++ fcn_courses.php
@@ -27,7 +27,7 @@
 	*/
 
 	# Sets db version.
-	$fcn_courses_db_version = '0.1' ;
+	$fcn_courses_db_version = '0.2' ;
 
 	# Sets base plugin path. With backslashes.
 	$plugin_path = plugin_dir_path(__FILE__) ;
