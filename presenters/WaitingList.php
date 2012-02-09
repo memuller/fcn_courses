bef596a6a6c20407a07032652e8227110f10e3a9
Matheus E. Muller <hello@memuller.com>
2012-02-09 13:46:15 -0200

    Adds WaitingList form and presenter logic.

--- presenters/WaitingList.php
+++ presenters/WaitingList.php
@@ -5,12 +5,25 @@
 	class WaitingListPresenter extends Presenter {
 		
 		public static function present(){
-			self::index ; 
+			if('POST' == $_SERVER['REQUEST_METHOD'] && !empty( $_POST['action'] ) && $_POST['action'] == 'waitee_signup'){
+				return self::post();
+			} else {
+				return self::index() ;
+			} 
 		}
 
 		public static function index(){
-			return self::render_to_string('waiting_list/form') ;
+			global $post ;
+			return self::render_to_string('waiting_list/form', array('post' => $post)) ;
 		}
+
+		public static function post(){
+			if(wp_verify_nonce($_POST['nonce'], 'waitee_signup')){
+				$waitee = Waitee::find_or_create($_POST['waitee']) ;
+				return self::render_to_string('waiting_list/form', array('success' => $waitee->new_record, 'waitee' => $waitee)) ;
+			}
+		}
+
 
 	}
 
