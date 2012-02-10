5de57a00a2a28c30dd9287ee17ecbad5ea642879
Matheus E. Muller <hello@memuller.com>
2012-02-10 05:14:14 -0200

    Adds admin page for WaitingList

--- presenters/CourseAdmin.php
+++ presenters/CourseAdmin.php
@@ -9,18 +9,27 @@
 			print_r($collumns) ;
 			return $collumns ;
 		}
+		
 		static function collumn_info($collumn_name, $post_id){
-			$course = new Course() ;
+			
 			switch ($collumn_name) {
+				
 				case 'waitee':
+					$course = new Course() ;
 					echo sizeof($course->waitees()) ;
 					break;
 			}
 		}
 
+		static function waiting_list_page(){
+			add_submenu_page('edit.php?post_type=courses', 'Waiting List', 'Waiting Lists',  
+				'edit_posts', 'waiting_list', 'FCN\WaitingListPresenter::present' ); 
+		}
+
 		static function build(){
 			add_filter( 'manage_edit-courses_columns', 'FCN\CourseAdminPresenter::collumn_headers', 10, 1);
 			add_action( 'manage_posts_custom_column', 'FCN\CourseAdminPresenter::collumn_info', 10, 2);
+			add_action( 'admin_menu', 'FCN\CourseAdminPresenter::waiting_list_page' ) ;
 		}
 	}
 
--- presenters/WaitingList.php
+++ presenters/WaitingList.php
@@ -7,6 +7,8 @@
 		public static function present(){
 			if('POST' == $_SERVER['REQUEST_METHOD'] && !empty( $_POST['action'] ) && $_POST['action'] == 'waitee_signup'){
 				return self::post();
+			} else if(is_admin()) {
+				echo self::admin_index();
 			} else {
 				return self::index() ;
 			} 
@@ -22,6 +24,18 @@
 				$waitee = Waitee::find_or_create($_POST['waitee']) ;
 				return self::render_to_string('waiting_list/form', array('success' => $waitee->new_record, 'waitee' => $waitee)) ;
 			}
+		}
+
+		public static function admin_index(){
+			global $post ; 
+			if(isset($_GET['course_id'])){
+				$post = get_post($_GET['course_id']) ;
+			} else {
+				$post = get_posts("post_type=courses&order_by=post_date&order=desc&limit=1") ;
+				$post = $post[0] ;
+			}
+
+			return self::render_to_string('admin/waiting_list', array('post' => $post)) ;
 		}
 
 
