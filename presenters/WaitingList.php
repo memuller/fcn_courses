91ef2a824ac86b31e8ae8befe67851db959eb453
Marcos Paiva <eu.marcos.paiva@gmail.com>
2012-02-15 14:07:57 -0200

    Estilização de formulário de inscrição

--- presenters/WaitingList.php
+++ presenters/WaitingList.php
@@ -16,7 +16,7 @@
 
 		public static function index(){
 			global $post ;
-			return self::render_to_string('waiting_list/form', array('post' => $post)) ;
+			return self::render_to_string('registry/form', array('post' => $post)) ;
 		}
 
 		public static function post(){
