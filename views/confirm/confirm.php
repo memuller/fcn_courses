e025bd058cade87f1fbba914fe93406b02a9c341
Matheus E. Muller <hello@memuller.com>
2012-02-20 11:30:26 -0200

    Converts confirmation form from HAML to HTML

---
+++ views/confirm/confirm.php
@@ -0,0 +1,55 @@
+<div id="confirm">
+	<h3> Olá
+		<span>Nome do cara</span>
+	</h3>
+	<p>
+		Você se cadastrou para o curso
+		<strong>Nome do curso</strong>
+		<br />
+		<br />
+		<strong>Dados para pagamento:</strong>
+		<small>
+			dados para depósito
+		</small>
+		Banco: Santander - 033
+		<br />
+		Agencia: 0143
+		<br />
+		Conta Corrente: 13-003554-3
+		<br />
+		Razao Social: FAT - Fundaçao de Apoio a Tecnologia
+		<br />
+		CNPJ: 58.415.092/0001-50
+	</p>
+	<ul id="steps" >
+		<li></li>
+	</ul>
+	<hr />
+	<h3>Resumo do seu cadastro</h3>
+	<p>
+		<strong>Nome:</strong>
+		Nome do anima;
+		<br />
+		<strong>Email</strong>
+		dfaspfoijsadfpoaisjdf
+		<br />
+		<strong>Curso</strong>
+		DSpdoiasjdoaijfposij
+	</p>
+	<hr />
+	<p>
+		<small>
+			*essas informações foram enviadas para o @email ('.
+			$values['e-mail'] .')
+		</small>
+		<br />
+		<small>
+			*em caso de dúvidas e-mail: faleconosco@fcn.edu.br ou
+			Telefone: (12) 3186 2000
+		</small>
+		<br />
+		<small>
+			*pressione ctrl+p para imprimir esse comprovante.
+		</small>
+	</p>
+</div>
