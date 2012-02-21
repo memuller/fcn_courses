d1767698107e36fd11351fa0a8b75728168f082d
Matheus E. Muller <hello@memuller.com>
2012-02-21 14:30:00 -0200

    Adds success/failure msgs to registry.

---
+++ views/registry/_registration_failure.php
@@ -0,0 +1,3 @@
+<div class="failure">
+  <?php _e("Sua inscrição falhou. Certifique-se que seu navegador suporta JavaScript, e que você já não se inscreveu para esta turma anteriormente.") ?>
+</div>
---
+++ views/registry/_registration_successful.php
@@ -0,0 +1,6 @@
+<div class="success">
+	<?php printf(__("Você se inscreveu para o curso %s"), $course->post_title ) ?>
+	<br>
+	<?php _e("Para confirmar sua inscrição, você precisa realizar o pagamento correspondente no prazo de 5 dias; 
+	você pode encontrar as instruções para fazê-lo abaixo e em um e-mail que lhe enviamos.") ?>
+</div>
