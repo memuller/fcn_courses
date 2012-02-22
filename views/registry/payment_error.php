ffd8ab30ad89254d19d94603db6c68b224c73778
Matheus E. Muller <hello@memuller.com>
2012-02-22 09:42:55 -0200

    Adds a view for use on failed payments.

---
+++ views/registry/payment_error.php
@@ -0,0 +1,9 @@
+<div class="failure">
+	<?php _e('Ocorreu uma falha ao processar seu pagamento. Algums possíveis motivos:') ?>
+	<ul>
+		<li><?php _e('Você tentou enviar um arquivo não suportado - somente imagens do tipo PNG, GIF ou JPG são suportados.') ?></li>
+		<li><?php _e('Você já realizou este pagamento - não é necessário pagar novamente.') ?></li>
+		<li><?php _e('O prazo para pagamento já se esgotou - você terá que se inscrever novamente, caso hajam vagas.') ?></li>
+		<li><?php _e('A turma na qual você se inscreveu já está fechada.') ?></li>
+	</ul>
+</div>
