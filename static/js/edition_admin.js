f1aedfb6ad0c22deb976a5deb3c2a94b3f59cedd
Matheus E. Muller <hello@memuller.com>
2012-02-23 00:54:15 -0200

    Kills token generated by wp_list_table.

--- static/js/edition_admin.js
+++ static/js/edition_admin.js
@@ -1,3 +1,8 @@
 jQuery(document).ready( function($) {
 	$('.inlineDatePicker').datepick()
+
+	// kills token from wp_list_table ; otherwise publishing classes won't work.
+	$('#publish').on('click', function(event){
+		$('#class-registrees #_wpnonce').remove();
+	});
 });
