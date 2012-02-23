1af37c7bc81028d5a88bb96b2d02d693328a2c7d
Matheus E. Muller <hello@memuller.com>
2012-02-23 19:49:47 -0200

    Adds a presenter skeleton for mailing.

---
+++ presenters/Mailer.php
@@ -0,0 +1,25 @@
+<?php 
+	namespace FCN ; 
+	use Presenter ;
+	
+	class MailerPresenter extends Presenter {
+
+		function payment_request($registree){
+
+		}
+
+		function payment_reminder(){
+
+		}
+
+		function payment_failure(){
+
+		}
+
+		function success(){
+
+		}
+
+	}
+
+ ?>
