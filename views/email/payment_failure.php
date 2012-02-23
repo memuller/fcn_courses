cc3d7616ce1b9df9c344d4faf12702abe8cd3151
Matheus E. Muller <hello@memuller.com>
2012-02-23 19:50:08 -0200

    Adds mail to be used when a payment fails.

---
+++ views/email/payment_failure.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+		<title></title>
+	</head>
+	<body style="margin:0; padding:0;">
+	<table border="0" cellspacing="0" cellpadding="0" style="width:600px;font-family:arial,sans-serif;">
+		<tr>
+			<th style="text-align:left;">
+				<img src="http://www.fcn.edu.br/wp-content/themes/faculdade/images/logotipo.png" alt="Faculdade Canção Nova">
+			</th>
+		</tr>
+		<tr>
+			<td>
+				<h1 style="color: #0883BE;font-size: 18px;font-weight: normal; margin-bottom: 12px; margin-top: 20px;">Olá %Nome do animal%</h1>
+				<p style="color:#333333;font-size:12px;line-height:150%;">
+					Você está recebendo esse email porque se inscreveu para o curso <strong>%nome do curso%</strong><br/>
+					Notamos que a sua inscrição ainda não foi efetivada em nosso sistema.<br/>
+					Para efetivar, acesse <a href="http://url">esse link</a> e siga as instruções.<br/>
+					Caso não deseje mais fazer o curso, favor desconsiderar esse email.
+				</p>
+				<hr>
+				<p style="color:#ff0000;font-size:12px;line-height:150%;">
+					<strong>IMPORTANTE</strong><br>
+					A sua inscrição irá expirar no prazo de 5 dias.
+				</p>
+				<hr>
+				<p style="color:#333333;font-size:12px;line-height:150%;">
+					*O não pagamento da taxa de inscrição implica na não efetivação da inscrição.<br>
+					*Em caso de dúvidas e-mail: faleconosco@fcn.edu.br ou Telefone: (12) 3186 2000 <br>
+					*Para solucionar qualquer dúvida, guarde com você o comprovante de depósito.
+				</p>
+			</td>
+		</tr>
+	</table>
+	</body>
+
+</html>
