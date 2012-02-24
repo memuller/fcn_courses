d3fc85cbd6f3e21fc247127effa58b4e754925a2
Marcos Paiva <eu.marcos.paiva@gmail.com>
2012-02-24 14:09:31 -0200

    Adicionando informações no painel administrativo

--- views/admin/class_information.php
+++ views/admin/class_information.php
@@ -1,3 +1,7 @@
-<?php 
-	echo "gueres!" ;
- ?>
+<p id="class_info">
+	<br>
+	<strong>Estatística dessa turma:</strong>
+	<span>5 </span>Pessoas se cadastraram, sendo que <span>4</span> ainda não efetivaram a inscrição. Ainda restam <span>56</span> vagas em aberto.<br>
+	<strong>Sobre a data</strong>
+	Faltam 40 dias para o fim das inscrições para esse curso
+</p>
