491ab6d89f386705a67488f69ce0f93c73930166
Matheus Elias Muller <hello@memuller.com>
2012-03-08 11:17:53 -0300

    Fixes jQuery versioning error.

--- static/js/payment_confirmation.js
+++ static/js/payment_confirmation.js
@@ -1,6 +1,6 @@
 jQuery(document).ready( function($) {
 
-	$('#payment-confirmation a').on('click', function(event){
+	$('#payment-confirmation a').live('click', function(event){
 		$('#text-paste').toggle('fast');
 		$('#file-upload').toggle('fast') ;
 	});
